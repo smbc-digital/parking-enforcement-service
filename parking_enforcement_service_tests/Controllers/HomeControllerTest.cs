243b8f344897fef3e30ce6cd600b15f883feeadd
michael.lau <michael.lau@stockport.gov.uk>
2020-05-18 14:42:21 +0100

    test: Add unit test for HomeController

---
+++ parking_enforcement_service_tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using parking_enforcement_service.Controllers;
+using parking_enforcement_service.Models;
+using parking_enforcement_service.Services;
+using StockportGovUK.NetStandard.Models.Addresses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace parking_enforcement_service_tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        private readonly HomeController _controller;
+        private readonly Mock<ILogger<HomeController>> _mockLogger = new Mock<ILogger<HomeController>>();
+        private readonly Mock<IParkingEnforcementService> _mockCrmService = new Mock<IParkingEnforcementService>();
+
+        public HomeControllerTest()
+        {
+            _controller = new HomeController(_mockLogger.Object, _mockCrmService.Object);
+        }
+        [Fact]
+        public async void PostCrmCase_ShouldCallParkingEnforcementService()
+        {
+            //Arrange
+            _mockCrmService
+                .Setup(_ => _.CreateCase(It.IsAny<ParkingEnforcementRequest>()))
+                .ReturnsAsync(It.IsAny<string>());
+
+            var Data = new ParkingEnforcementRequest
+            {
+                FirstName = "Joe",
+                LastName = "Bloggs",
+                Email = "joe@test.com",
+                Phone = "0161 123 1234",
+                MoreDetails = "details",
+                FurtherInformation = "further info",
+
+                StreetAddress = new Address
+                {
+                    SelectedAddress = "Test",
+                    AddressLine1 = "100 Green road",
+                    AddressLine2 = "",
+                    Town = "Stockport",
+                    Postcode = "SK2 9FT",
+                    PlaceRef = "",
+                },
+
+                CustomersAddress = new Address
+                {
+                    SelectedAddress = "Test",
+                    AddressLine1 = "100 Green road",
+                    AddressLine2 = "",
+                    Town = "Stockport",
+                    Postcode = "SK2 9FT",
+                    PlaceRef = "",
+                }
+            };
+
+            // Act
+            await _controller.Post(Data);
+
+            // Assert
+            _mockCrmService.Verify(_ => _.CreateCase(It.IsAny<ParkingEnforcementRequest>()), Times.Once);
+        }
+    }
+}
