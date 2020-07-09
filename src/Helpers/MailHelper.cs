65ed4e89e9e3ed58e53c93e4c07d90ffe71c3451
Colin Lees <colinleesa7x@gmail.com>
2020-07-09 10:45:56 +0100

    fix: remove unneeded ComplaintsMailModel

--- src/Helpers/MailHelper.cs
+++ src/Helpers/MailHelper.cs
@@ -1,7 +1,6 @@
 ﻿using parking_enforcement_service.Models;
 using Newtonsoft.Json;
 using StockportGovUK.NetStandard.Gateways.MailingServiceGateway;
-using StockportGovUK.NetStandard.Models.ComplimentsComplaints;
 using StockportGovUK.NetStandard.Models.Mail;
 using StockportGovUK.NetStandard.Models.Enums;
 
@@ -18,19 +17,16 @@ namespace parking_enforcement_service.Helpers
 
         public void SendEmail(Person person, EMailTemplate template, string caseReference)
         {
-            var submissionDetails = new ComplaintsMailModel
-            {
-                Subject = "Parking enforcement request form - submission",
-                Reference = caseReference,
-                FirstName = person.FirstName,
-                LastName = person.LastName,
-                RecipientAddress = person.Email
-            };
-
             _mailingServiceGateway.Send(new Mail
             {
-                Payload = JsonConvert.SerializeObject(submissionDetails),
-                Template = template
+                Template = template,
+                Payload = JsonConvert.SerializeObject(new {
+                    Subject = "Parking enforcement request form - submission",
+                    Reference = caseReference,
+                    FirstName = person.FirstName,
+                    LastName = person.LastName,
+                    RecipientAddress = person.Email
+                })
             });
         }
     }
